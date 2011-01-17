3b7d25c89fea30473d6c6664c5af9cf60c83100e
c9s <cornelius.howl@gmail.com>
2011-01-17 23:29:34 +0800

    module setup

---
+++ lib/App/cpanbaker.pm
@@ -0,0 +1,32 @@
+package App::cpanbaker;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::cpanbaker -
+
+=head1 SYNOPSIS
+
+  use App::cpanbaker;
+
+=head1 DESCRIPTION
+
+App::cpanbaker is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
